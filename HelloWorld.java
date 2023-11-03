8ad265a90e9b506dabd65b39fec4698f9b777e2a
DILSHAN565 <65005789+DILSHAN565@users.noreply.github.com>
2023-11-03 14:59:03 +0530

    first

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World lukluk changes This is your one ");
+        System.out.println("Ehemada Asa oyala ");
+        System.out.println("Just do it");
+        System.out.println("Beep Beep");
+        System.out.println("Beep Beep 2");
+         System.out.println("Beep Beep  3");
+        System.out.println("MOOSE MOOSE 3");
+         System.out.println("This is ne okay okay");
+          System.out.println("HI Hi HI 3");
+
+    }
+}
